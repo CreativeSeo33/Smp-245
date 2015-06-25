f3306fd0a997a0f31a8a1d41a250245b0d620add
CreativeSeo33 <alexeyfedotof@gmail.com>
2015-06-25 17:05:44 +0300

    Contacts

--- contacts/index.php
+++ contacts/index.php
@@ -1,9 +1,11 @@
 <?php $title = 'Контакты - СМП 245'; ?>
 <?php $description = ''; ?>
 <?php $keywords = ''; ?>
-<?php $root = $_SERVER['DOCUMENT_ROOT'];
-include("../blocks/meta.php");
-include("../blocks/header.php"); ?>
+<?php 
+$root = $_SERVER['DOCUMENT_ROOT'];
+include($root."/blocks/meta.php");
+include($root."/blocks/header.php"); 
+?>
 
 <article>
 <div class="container">
