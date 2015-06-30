2f38468c48d314d123af414cd2896f8f16ec5eb2
Alex Illar <cgde88@gmail.com>
2015-06-30 14:54:06 +0300

    Аренда спецтехники
    Добавлены автобусы и грузовые автомобили

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php 
-$host = $_SERVER['HTTP_HOST'];
-$self = explode("/", $_SERVER['PHP_SELF']);
-define('HTTP_SERVER', 'http://'.$host.'/');
+$self = explode("/", $_SERVER['REQUEST_URI']);
+define('HTTP_SERVER', 'http://'.$_SERVER['HTTP_HOST'].'/');
+define('HTTP_ID', $self[count($self)-2].'/');
+define('HTTP_URL', HTTP_SERVER.ltrim($_SERVER['REQUEST_URI'], '/'));
+define('HTTP_URL_IMG', HTTP_URL.'img/');
 ?>
