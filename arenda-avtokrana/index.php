3c1e105b0db634faf3bf2a16762b552ba5ec2d95
Alex Illar <cgde88@gmail.com>
2015-07-23 18:57:36 +0300

    u+

--- arenda-avtokrana/index.php
+++ arenda-avtokrana/index.php
@@ -9,7 +9,7 @@ $array=json_decode('[
 {"name":"Кран на а/ходу КС-45717-К","url":"kran-na-hodu-ks-45717-k"},
 {"name":"Кран на а/ходу XZJ QZ25K","url":"kran-na-hodu-xzj-qz25k"},
 {"name":"Кран манипулятор автомобильный на базе КАМАЗ-65117","url":"kran-manipulyator-na-kamaz-65117"},
-{"name":"Кран на ж. д. ходу ЕДК-80/3":"kran-na-hodu-edk-803"},
+{"name":"Кран на ж. д. ходу ЕДК-80/3","url":"kran-na-hodu-edk-803"},
 {"name":"Кран на ж. д. ходу КДЭ-163 с грейфером","url":"kran-na-hodu-kde-163-s-greyferom"}
 ]',true);
 include("../project.php"); ?>
